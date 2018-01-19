f2651510049c3d55767f803425c943509ff6dfbf
John Urbani <johnathanurbani@gmail.com>
2018-01-19 16:52:53 -0500

    Another test

---
+++ 2018-Robot/src/org/usfirst/frc/team2370/robot/OI.java
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team2370.robot;
+
+import org.usfirst.frc.team2370.robot.commands.ExampleDriveCommand;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+/**
+ * Operator interface
+ */
+public class OI {
+	public static Joystick stick;
+		
+	public static void init(){
+		stick = new Joystick(0);
+		Button button = new JoystickButton(stick, 1);
+			
+		button.whenPressed(new ExampleDriveCommand());
+
+		button.whileHeld(new ExampleDriveCommand());
+		
+		button.whenReleased(new ExampleDriveCommand());
+	}
+}
