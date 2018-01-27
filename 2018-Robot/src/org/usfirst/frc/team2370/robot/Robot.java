49eba276cb224778a23013a53780b3005bae029e
Administrator <Administrator@user-PC>
2018-01-27 13:41:35 -0500

    whatever

--- 2018-Robot/src/org/usfirst/frc/team2370/robot/Robot.java
+++ 2018-Robot/src/org/usfirst/frc/team2370/robot/Robot.java
@@ -36,26 +36,20 @@ import org.usfirst.frc.team2370.robot.subsystems.Vision;
  * project.
  */
 public class Robot extends TimedRobot {
-	public static final DriveTrain kDriveTrain
-			= new DriveTrain();
-	public static final Gripper kGripper
-			= new Gripper();
-	public static final Elevator kElevator
-			= new Elevator();
-	public static final Ramps kRamps
-			= new Ramps();
-	public static final Vision kVision
-			= new Vision();
-	public static final Dashboard kDashboard
-			= new Dashboard();
+	public static final DriveTrain kDriveTrain = new DriveTrain();
+	public static final Gripper kGripper = new Gripper();
+	public static final Elevator kElevator = new Elevator();
+	public static final Ramps kRamps = new Ramps();
+	public static final Vision kVision = new Vision();
+	public static final Dashboard kDashboard = new Dashboard();
 	public static OI m_oi;
 
 	Command m_autonomousCommand;
 	SendableChooser<Command> m_chooser = new SendableChooser<>();
 
 	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
+	 * This function is run when the robot is first started up and should be used
+	 * for any initialization code.
 	 */
 	@Override
 	public void robotInit() {
@@ -67,9 +61,9 @@ public class Robot extends TimedRobot {
 	}
 
 	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
+	 * This function is called once each time the robot enters Disabled mode. You
+	 * can use it to reset any subsystem information you want to clear when the
+	 * robot is disabled.
 	 */
 	@Override
 	public void disabledInit() {
@@ -83,24 +77,25 @@ public class Robot extends TimedRobot {
 
 	/**
 	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString code to get the auto name from the text box below the Gyro
+	 * between different autonomous modes using the dashboard. The sendable chooser
+	 * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+	 * remove all of the chooser code and uncomment the getString code to get the
+	 * auto name from the text box below the Gyro
 	 *
-	 * <p>You can add additional auto modes by adding additional commands to the
-	 * chooser code above (like the commented example) or additional comparisons
-	 * to the switch structure below with additional strings & commands.
+	 * <p>
+	 * You can add additional auto modes by adding additional commands to the
+	 * chooser code above (like the commented example) or additional comparisons to
+	 * the switch structure below with additional strings & commands.
 	 */
 	@Override
 	public void autonomousInit() {
 		m_autonomousCommand = m_chooser.getSelected();
 
 		/*
-		 * String autoSelected = SmartDashboard.getString("Auto Selector",
-		 * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-		 * = new MyAutoCommand(); break; case "Default Auto": default:
-		 * autonomousCommand = new ExampleCommand(); break; }
+		 * String autoSelected = SmartDashboard.getString("Auto Selector", "Default");
+		 * switch(autoSelected) { case "My Auto": autonomousCommand = new
+		 * MyAutoCommand(); break; case "Default Auto": default: autonomousCommand = new
+		 * ExampleCommand(); break; }
 		 */
 
 		// schedule the autonomous command (example)
--- 2018-Robot/src/org/usfirst/frc/team2370/robot/subsystems/Vision.java
+++ 2018-Robot/src/org/usfirst/frc/team2370/robot/subsystems/Vision.java
@@ -17,7 +17,6 @@ public class Vision extends Subsystem {
 	// here. Call these from Commands.
 
 	public void initDefaultCommand() {
-		// Set the default command for a subsystem here.
-		// setDefaultCommand(new MySpecialCommand());
+		
 	}
 }
