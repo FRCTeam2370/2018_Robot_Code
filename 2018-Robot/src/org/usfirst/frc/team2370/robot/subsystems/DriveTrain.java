25ed32e03aebf34092d2a57e56ed7562285c6b2a
John Urbani <johnathanurbani@gmail.com>
2018-02-01 16:09:29 -0500

    Outline started, empty methods to be finished in DriveTrain

--- 2018-Robot/src/org/usfirst/frc/team2370/robot/subsystems/DriveTrain.java
+++ 2018-Robot/src/org/usfirst/frc/team2370/robot/subsystems/DriveTrain.java
@@ -22,24 +22,43 @@ public class DriveTrain extends Subsystem {
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
 
+	/**
+	 * Method to setup the slave speed controllers to follower mode
+	 */
 	public static void motorSetup() {
 		RobotMap.TAL_leftSlave.follow(RobotMap.TAL_leftMaster);
 		RobotMap.TAL_rightSlave.follow(RobotMap.TAL_rightMaster);
 	}
 
+	/**
+	 * Use arcade drive from the driveTrain using a speed and rotation parameter
+	 * @param speed - The speed (forward and backward) of the speed controllers (-1.0 to 1.0)
+	 * @param rotation - The rotation (left and right) of the speed controllers (-1.0 to 1.0)
+	 */
 	public static void arcadeDrive(double speed, double rotation) {
 		RobotMap.driveTrain.arcadeDrive(speed, rotation);
 	}
 
+	/**
+	 * A method to stop all drive motors for use at the end of some commands
+	 */
 	public static void stopMotors() {
 		RobotMap.TAL_rightMaster.set(0);
 		RobotMap.TAL_leftMaster.set(0);
 	}
 
+	/**
+	 * A method to turn the robot to a specific angle based on accelerometer
+	 * @param angle - The angle the robot will turn to
+	 */
 	public static void turn(double angle) {
 
 	}
 	
+	/**
+	 * A method to drive the robot forward a specific distance (In inches) using encoders
+	 * @param distance - The distance (In inches) to drive forward
+	 */
 	public static void driveStraight(double distance) {
 
 	}
