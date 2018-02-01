427b28550f6a8ce334d132677f2e9066b9356d5b
John Urbani <johnathanurbani@gmail.com>
2018-02-01 15:59:10 -0500

    Start of documentation

--- 2018-Robot/src/org/usfirst/frc/team2370/robot/RobotMap.java
+++ 2018-Robot/src/org/usfirst/frc/team2370/robot/RobotMap.java
@@ -38,30 +38,35 @@ import edu.wpi.first.wpilibj.networktables.NetworkTable;
 public class RobotMap {
 	
 	/**
-	 * This will set the Solenoids for the shifting + the compressor
+	 * This will set the solenoids and compressor for the Pneumatics subsystem
 	 */
 	public static Solenoid SLN_rightSolenoid1 = new Solenoid(0);
 	public static Solenoid SLN_rightSolenoid2 = new Solenoid(1);
-	
-	
 	public static Compressor compressor = new Compressor();
 
+	/**
+	 * This will set the speed controllers, drive train object, and deadband for the DriveTrain subsystem
+	 */
 	public static WPI_TalonSRX TAL_rightMaster = new WPI_TalonSRX(16);
 	public static WPI_TalonSRX TAL_leftMaster = new WPI_TalonSRX(17);
 	public static WPI_TalonSRX TAL_rightSlave = new WPI_TalonSRX(18);
 	public static WPI_TalonSRX TAL_leftSlave = new WPI_TalonSRX(19);
 	public static DifferentialDrive driveTrain = new DifferentialDrive(TAL_leftMaster, TAL_rightMaster);
-
-	//public static WPI_TalonSRX TAL_gripMotorLeft = new WPI_TalonSRX(0);
-	//public static WPI_TalonSRX TAL_gripMotorRight = new WPI_TalonSRX(0);
-	//public static WPI_TalonSRX TAL_elevatorMotor = new WPI_TalonSRX(12);
-	/* 
-	 * //public static DigitalInput testDigitalInput = new DigitalInput(0); //public
-	 * static DigitalOutput testDigitalOutput = new DigitalOutput(0);
+	public static double deadbandPercent = 0;
+	
+	/**
+	 * This will set the speed controllers for the Gripper subsystem
+	 */
+	public static WPI_TalonSRX TAL_gripMotorLeft = new WPI_TalonSRX(0);
+	public static WPI_TalonSRX TAL_gripMotorRight = new WPI_TalonSRX(0);
+	
+	/**
+	 * This will set the speed controller for the Elevator subsystem
+	 */
+	public static WPI_TalonSRX TAL_elevatorMotor = new WPI_TalonSRX(12);
+	
+	/**
 	 * 
-	 * public static Compressor compressor = new Compressor(0); public static
-	 * Solenoid gearSolenoid1 = new Solenoid(0); public static Solenoid
-	 * gearSolenoid2 = new Solenoid(1);
 	 */
 	public static NetworkTable limeLightTable = NetworkTable.getTable("limelight");
 	public static UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
