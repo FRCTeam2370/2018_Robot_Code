8c2f7c366608bdad368e53aa8f6e56e0178518fb
2370 User <2370 User@10.33.90.193>
2018-02-06 18:36:11 -0500

    Added LEDChangeColor command and associated chooser, Good day sir

--- 2018-Robot/src/org/usfirst/frc/team2370/robot/Robot.java
+++ 2018-Robot/src/org/usfirst/frc/team2370/robot/Robot.java
@@ -55,6 +55,9 @@ public class Robot extends TimedRobot {
 	Command m_autonomousCommand;
 	SendableChooser<Command> m_chooser = new SendableChooser<>();
 
+	public static Command m_LEDCommand;
+	SendableChooser<Command> LED_chooser = new SendableChooser<>();
+	
 	/**
 	 * This function is run when the robot is first started up and should be used
 	 * for any initialization code.
