654a89ffb8a5044d6fd403b82f69e7d71f01f7a1
Administrator <Administrator@10.33.90.41>
2018-02-18 17:18:55 -0500

    Autonomous Grouping

--- 2018-Robot/src/org/usfirst/frc/team2370/robot/commands/PushElevatorSol.java
+++ 2018-Robot/src/org/usfirst/frc/team2370/robot/commands/PushElevatorSol.java
@@ -29,7 +29,7 @@ public class PushElevatorSol extends Command {
 
 	// Called repeatedly when this Command is scheduled to run
 	@Override
-	protected void execute() {
+	protected void execute() { 
 		RobotMap.SLN_elevatorSolenoid.set(false);
 	}
 
