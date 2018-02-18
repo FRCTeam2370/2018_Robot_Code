c45ea539606b4ce49112e1859b5984d06250ed2a
2370 User <2370 User@10.33.90.69>
2018-02-18 18:33:13 -0500

    aaa

--- 2018-Robot/src/org/usfirst/frc/team2370/robot/commands/ElevatorToBottom.java
+++ 2018-Robot/src/org/usfirst/frc/team2370/robot/commands/ElevatorToBottom.java
@@ -36,7 +36,7 @@ public class ElevatorToBottom extends Command {
 
 	// Called once after isFinished returns true
 	protected void end() {
-	}
+	} 
 
 	// Called when another command which requires one or more of the same
 	// subsystems is scheduled to run
