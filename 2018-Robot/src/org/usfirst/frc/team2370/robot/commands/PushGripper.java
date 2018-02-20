b86aa4ff3b6384b42883e2bc4676fde39dfad2f2
Administrator <Administrator@10.33.90.35>
2018-02-20 14:50:56 -0500

    Added More to Autonomous

--- 2018-Robot/src/org/usfirst/frc/team2370/robot/commands/PushGripper.java
+++ 2018-Robot/src/org/usfirst/frc/team2370/robot/commands/PushGripper.java
@@ -30,7 +30,7 @@ public class PushGripper extends Command {
 	// Called repeatedly when this Command is scheduled to run
 	@Override
 	protected void execute() {
-		Gripper.pullPush(.75);
+		Gripper.pullPush(.5);
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
